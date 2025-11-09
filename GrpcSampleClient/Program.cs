2cece0b3476e398a3b30331e780b41e4dc55d6f2
Vaishnavi Kanagaraj <vaishnavi.kanagaraj@groupm.com>
2025-11-09 16:14:50 +0530

    Add project files.
    (cherry picked from commit 4b8baf979c0930d1976ec610e19c20c6a1e385a9)

---
+++ GrpcSampleClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcServer;  // Adjust the namespace accordingly from your .proto file
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+
+        var httpHandler = new HttpClientHandler();
+        httpHandler.ServerCertificateCustomValidationCallback =
+            HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+        using var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions
+        {
+            HttpHandler = httpHandler
+        });
+
+        // Create the gRPC client from the generated code
+        var client = new Greeter.GreeterClient(channel);
+
+        // Call the SayHello RPC method on the server and await the response
+        var reply = await client.SayHelloAsync(new HelloRequest { Name = "User" });
+
+        // Print the server reply message to console
+        Console.WriteLine("Reply from server: " + reply.Message);
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadLine();
+
+    }
+}
---
+++ GrpcServer/GrpcServer/Program.cs
@@ -0,0 +1,20 @@
+using GrpcServer.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenLocalhost(5001, listenOptions =>
+    {
+        listenOptions.UseHttps();
+    });
+});
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", () => "Use a gRPC client.");
+app.Run();
